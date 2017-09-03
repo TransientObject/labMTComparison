536bf3e4eeac72d44214c797f3c1950648b6df47
Priya PNS <mail2priyanarayanan@gmail.com>
2017-09-02 22:44:32 -0700

    new travel lang and MSE with lemmatization

--- verify_results.py
+++ verify_results.py
@@ -2,40 +2,95 @@ import csv
 from labMTsimple import storyLab
 import re
 from collections import defaultdict
+from nltk.stem.wordnet import WordNetLemmatizer
+
 
 class VerifyResult(object):
+
+    def lemmatized_word(self, word):
+        if word[0] in ['#', '@']:
+            return self.lmtzr.lemmatize(word[1:])
+        return self.lmtzr.lemmatize(word)
 
     def __init__(self):
         with open('meanscores.csv', 'r') as csvfile:
             self.wsudata = list(csv.reader(csvfile, delimiter=','))
 
         self.wsudict = defaultdict(float)
+        self.lmtzr = WordNetLemmatizer()
+        lemmatized_list = defaultdict(lambda: [])
+
         for row in self.wsudata:
             self.wsudict[row[0].lower()] = float(row[1])
+            lemmatized_list[self.lemmatized_word(row[0].lower())].append(row[0].lower())
 
-        with open('toverify.csv', 'r') as csvfile:
+        # recompute sentiment values by calculating average of all non-lemmatized word that belong to a lemmatized word
+        for key, values in lemmatized_list.items():
+            if len(values) > 1:
+                lst = [self.wsudict[value] for value in values]
+                average_sentiment_value = sum(lst)/len(lst)
+                for value in values:
+                    #print(",".join([value, str(self.wsudict[value]), str(average_sentiment_value), str(abs(self.wsudict[value]-average_sentiment_value))]))
+                    self.wsudict[value] = average_sentiment_value
+
+        #for key, value in lemmatized_list.items():
+            #if len(value) > 1:
+                #print(key, " : ", value )
+
+        with open('toverify2.csv', 'r') as csvfile:
             self.inputfile = list(csv.reader(csvfile, delimiter=','))
         lang = 'english'
         self.labMTData, self.labMTSentiment, self.labMTwordList = storyLab.emotionFileReader(stopval=0.0, lang=lang, returnVector=True)
 
+        lemmatized_list_labmt = defaultdict(lambda: [])
+        for word in self.labMTwordList:
+            lemmatized_list_labmt[self.lemmatized_word(word.lower())].append(word.lower())
 
-c = VerifyResult()
-intersection_list = []
-skiprow = True
-for row in c.inputfile:
-    if (skiprow):
-        skiprow = False
-        continue
-    sum = 0
-    count = 0
+        # recompute sentiment values by calculating average of all non-lemmatized word that belong to a lemmatized word
+        for key, values in lemmatized_list_labmt.items():
+            if len(values) > 1:
+                lst = [float(self.labMTData[value][1]) for value in values]
+                average_sentiment_value = sum(lst) / len(lst)
+                for value in values:
+                    # print(",".join([value, str(self.wsudict[value]), str(average_sentiment_value), str(abs(self.wsudict[value]-average_sentiment_value))]))
+                    self.labMTData[value][1] = average_sentiment_value
 
-    tweet = re.sub(r'\W+', ' ', row[0]).lower()
-    for word in tweet.split(' '):
-        sum += c.wsudict[word]
-        if (c.wsudict[word] > 0):
-            count += 1
+        #for key, value in lemmatized_list_labmt.items():
+            #if len(value) > 1:
+                #print(key, " : ", value)
 
-    if (count > 0):
-        print(row[0].lower(), sum/count)
+def GenerateTweetSentimentValues(combined = False):
+    c = VerifyResult()
+    intersection_list = []
+    skiprow = True
+    for row in c.inputfile:
+        if (skiprow):
+            skiprow = False
+            continue
+        sum = 0
+        count = 0
+
+        tweet = re.sub(r'\W+', ' ', row[0].strip()).lower().strip()
+        for word in tweet.split(' '):
+            try:
+                sum += c.wsudict[word]
+                if (c.wsudict[word] > 0):
+                    count += 1
+                elif (c.wsudict[c.lemmatized_word(word)] > 0):
+                    sum += c.wsudict[c.lemmatized_word(word)]
+                    count+=1
+                elif (combined and word in c.labMTData.keys() and float(c.labMTData[word][1]) > 0):
+                    sum += float(c.labMTData[word][1])
+                    count += 1
+                elif (combined and c.lemmatized_word(word) in c.labMTData.keys() and float(c.labMTData[c.lemmatized_word(word)][1]) > 0):
+                    sum += float(c.labMTData[c.lemmatized_word(word)][1])
+                    count += 1
+            except Exception as exc:
+                print(exc)
+                #print("tweet - ", tweet, "\n", "word - ", tweet.split(' '), "\n")
+
+        if (count > 0):
+            print(sum/count)
 
 
+GenerateTweetSentimentValues(True)
