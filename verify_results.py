e3cccd3e6ac9217f231b8be4d9d1b66fb8dd7cf8
Priya Narayana Subramanian <mail2priyanarayanan@gmail.com>
2017-09-07 16:37:50 -0700

    updated excel and plots

--- verify_results.py
+++ verify_results.py
@@ -9,14 +9,11 @@ import matplotlib.pyplot as plt
 class SentimentDict(object):
 
     def lemmatized_word(self, word):
-        return self.lmtzr.lemmatize(word)
         if word[0] in ['#', '@']:
             return word[1:]
         return word
-            #return self.lmtzr.lemmatize(word[1:])
-        return self.lmtzr.lemmatize(word)
 
-    def __init__(self, optimize_labmt=True):
+    def __init__(self, optimize_dict=True):
         with open('meanscores.csv', 'r') as csvfile:
             self.wsudata = list(csv.reader(csvfile, delimiter=','))
 
@@ -26,36 +23,31 @@ class SentimentDict(object):
 
         for row in self.wsudata:
             self.wsudict[row[0].lower()] = float(row[1])
-            lemmatized_list[self.lemmatized_word(row[0].lower())].append(row[0].lower())
+            if (optimize_dict):
+                lemmatized_list[self.lemmatized_word(row[0].lower())].append(row[0].lower())
+
+        self.labMTData, self.labMTSentiment, self.labMTwordList = storyLab.emotionFileReader(stopval=0.0, lang='english', returnVector=True)
+
+        with open('toverify2.csv', 'r') as csvfile:
+            self.inputfile = list(csv.reader(csvfile, delimiter=','))
+
+        # for key, value in lemmatized_list.items():
+        # if len(value) > 1:
+        # print(key, " : ", value )
+        # if len(value) == 1 and value[0] != key:
+        # print(key, " : ", value)
 
         # recompute sentiment values by calculating average of all non-lemmatized word that belong to a lemmatized word
-        for key, values in lemmatized_list.items():
-            if len(values) > 1:
-                lst = [self.wsudict[value] for value in values]
-                average_sentiment_value = sum(lst)/len(lst)
-                for value in values:
-                    #print(",".join([value, str(self.wsudict[value]), str(average_sentiment_value), str(abs(self.wsudict[value]-average_sentiment_value))]))
-                    self.wsudict[value] = average_sentiment_value
-
-        #for key, value in lemmatized_list.items():
-            #if len(value) > 1:
-                #print(key, " : ", value )
-            #if len(value) == 1 and value[0] != key:
-                #print(key, " : ", value)
-
-        if (optimize_labmt):
+        if (optimize_dict):
+            for key, values in lemmatized_list.items():
+                if len(values) > 1:
+                    lst = [self.wsudict[value] for value in values]
+                    average_sentiment_value = sum(lst)/len(lst)
+                    for value in values:
+                        #print(",".join([value, str(self.wsudict[value]), str(average_sentiment_value), str(abs(self.wsudict[value]-average_sentiment_value))]))
+                        self.wsudict[value] = average_sentiment_value
 
             lemmatized_list_labmt = defaultdict(lambda: [])
-
-            with open('toverify2.csv', 'r') as csvfile:
-                self.inputfile = list(csv.reader(csvfile, delimiter=','))
-            lang = 'english'
-            self.labMTData, self.labMTSentiment, self.labMTwordList = storyLab.emotionFileReader(stopval=0.0, lang=lang,
-                                                                                                 returnVector=True)
-
-            for word in self.labMTwordList:
-                if word.lower() != self.lemmatized_word(word.lower()):
-                    lemmatized_list_labmt[self.lemmatized_word(word.lower())].append(word.lower())
 
             for word in self.labMTwordList:
                 lemmatized_list_labmt[self.lemmatized_word(word.lower())].append(word.lower())
@@ -69,44 +61,80 @@ class SentimentDict(object):
                         # print(",".join([value, str(self.wsudict[value]), str(average_sentiment_value), str(abs(self.wsudict[value]-average_sentiment_value))]))
                         self.labMTData[value][1] = average_sentiment_value
 
-            #for key, value in lemmatized_list_labmt.items():
-                #if len(value) > 1:
-                    #print(key, " : ", value)
+    def GetWSUSentimentScore(self, tweet):
+        sum = 0
+        count = 0
+        for word in tweet.split(' '):
+            if (self.wsudict[word] > 0):
+                sum += self.wsudict[word]
+                count += 1
 
-def GenerateTweetSentimentValues(combined = False):
-    c = SentimentDict(True)
-    intersection_list = []
+        if (count > 0):
+            return(sum/count)
+        return 0
+
+    def GetLabMTSentimentScore(self, tweet):
+        sum = 0
+        count = 0
+        for word in tweet.split(' '):
+            if (word in self.labMTData.keys() and float(self.labMTData[word][1]) > 0):
+                sum += float(self.labMTData[word][1])
+                count += 1
+
+        if (count > 0):
+            return(sum/count)
+        return 0
+
+    def GetCombinedSentimentScore(self, tweet):
+        sum = 0
+        count = 0
+        for word in tweet.split(' '):
+            if (self.wsudict[word] > 0):
+                sum += self.wsudict[word]
+                count += 1
+            elif (word in self.labMTData.keys() and float(self.labMTData[word][1]) > 0):
+                sum += float(self.labMTData[word][1])
+                count += 1
+
+        if (count > 0):
+            return(sum/count)
+        return 0
+
+    def GetTravelMTSentimentScore(self, tweet):
+        sum = 0
+        count = 0
+        for word in tweet.split(' '):
+            word = self.lemmatized_word(word)
+            if (self.wsudict[word] > 0):
+                sum += self.wsudict[word]
+                count += 1
+            elif (word in self.labMTData.keys() and float(self.labMTData[word][1]) > 0):
+                sum += float(self.labMTData[word][1])
+                count += 1
+
+        if (count > 0):
+            return (sum / count)
+        return 0
+
+def GenerateTweetSentimentValues(mode = 'wsu', optimize_dict = False):
+    c = SentimentDict(optimize_dict)
     skiprow = True
     for row in c.inputfile:
         if (skiprow):
             skiprow = False
             continue
-        sum = 0
-        count = 0
 
         tweet = re.sub(r'\W+', ' ', row[0].strip()).lower().strip()
-        for word in tweet.split(' '):
-            try:
-                if (c.wsudict[word] > 0):
-                    sum += c.wsudict[word]
-                    count += 1
-                elif (c.wsudict[c.lemmatized_word(word)] > 0):
-                    sum += c.wsudict[c.lemmatized_word(word)]
-                    count+=1
-                elif (combined and word in c.labMTData.keys() and float(c.labMTData[word][1]) > 0):
-                    sum += float(c.labMTData[word][1])
-                    count += 1
-                elif (combined and c.lemmatized_word(word) in c.labMTData.keys() and float(c.labMTData[c.lemmatized_word(word)][1]) > 0):
-                    sum += float(c.labMTData[c.lemmatized_word(word)][1])
-                    count += 1
-            except Exception as exc:
-                print(exc)
+        if (mode == 'wsu'):
+            print(c.GetWSUSentimentScore(tweet))
+        elif (mode == 'labmt'):
+            print(c.GetLabMTSentimentScore(tweet))
+        elif (mode == 'combined'):
+            print(c.GetCombinedSentimentScore(tweet))
+        elif (mode == 'travelmt'):
+            print(c.GetTravelMTSentimentScore(tweet))
 
-        if (count > 0):
-            print(sum/count)
-
-
-GenerateTweetSentimentValues(True)
+#GenerateTweetSentimentValues('travelmt', True)
 
 def GenerateCSVOfLabMTWSUIntersection():
     dic = SentimentDict()
@@ -137,10 +165,10 @@ def GenerateHistogram():
         if skipRow:
             skipRow = False
             continue
-        plot_input_list.append([float(x) for x in row[1:]])
+        plot_input_list.append([float(x) for x in row[4:]])
 
-    colors = ['red', 'green', 'blue']
-    labels = ['labmt', 'wsu', 'combined']
+    colors = ['red', 'green']
+    labels = ['labmt', 'travelmt']
     plt.hist(np.array(plot_input_list), 9, normed=1, histtype='bar', color=colors, label=labels)
     plt.legend(prop={'size': 9})
     plt.title('histogram with sentiment values in different dictionary binned by the sentiment score')
@@ -148,7 +176,7 @@ def GenerateHistogram():
 
 #GenerateHistogram()
 
-def GenerateScatterPlot():
+def GenerateScatterPlot(x_index, y_index, x_label, y_label):
     with open('sentiment_scores.csv', 'r') as csvfile:
         sentiment_data = list(csv.reader(csvfile, delimiter=','))
 
@@ -159,14 +187,22 @@ def GenerateScatterPlot():
         if skipRow:
             skipRow = False
             continue
-        plot_x.append(float(row[1]))
-        plot_y.append(float(row[3]))
+        plot_x.append(float(row[x_index]))
+        plot_y.append(float(row[y_index]))
 
     plt.scatter(plot_x, plot_y, alpha=0.5)
     plt.legend(prop={'size': 9})
-    plt.xlabel("LabMT Sentiment")
-    plt.ylabel("Combined Sentiment")
-    plt.title('How the 130 tweets are scattered against labmt and combined sentiment values')
+    plt.plot([int(min(plot_x))-1,int(max(plot_x))+1], [int(min(plot_x))-1,int(max(plot_x))+1])
+    plt.plot([int(min(plot_x)) - 1, int(max(plot_x))], [int(min(plot_x)), int(max(plot_x)) + 1])
+    plt.plot([int(min(plot_x)), int(max(plot_x)) + 1], [int(min(plot_x)) - 1, int(max(plot_x))])
+    #plt.plot([0,9], [0,9])
+    #plt.plot([0,8], [1,9])
+    #plt.plot([1,9], [0,8])
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title("How the 130 tweets are scattered against "+ x_label +" and "+ y_label + " sentiment values")
     plt.show()
 
-#GenerateScatterPlot()
+GenerateScatterPlot(1,5, "Human", "TravelMT")
+GenerateScatterPlot(2,5, "LabMT", "TravelMT")
+GenerateScatterPlot(1,2, "Human", "LabMT")
